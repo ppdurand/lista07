6ec6fa318de8483c9445c32ef0cc5440d1414652
Pedro Paulo Durand <72530488+ppdurand@users.noreply.github.com>
2022-07-15 19:25:12 -0300

    questão 3

---
+++ q03.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAM 3
+
+typedef struct{
+  int X, Y;
+  float Z;
+}node;
+
+int main(void) {
+
+    node *pStruct;
+
+    pStruct = malloc(TAM * sizeof(node));
+
+    if(!pStruct){
+        puts("Nao foi possivel alocar o ponteiro");
+        exit(1);
+    }
+
+    pStruct->X = 10;
+    pStruct->Y = 11;
+    pStruct->Z = (float)(pStruct->X + pStruct->Y)/2;
+    (pStruct+1)-> X = 20;
+    (pStruct+1)-> X = 27;
+    (pStruct+1)->Z = (float)((pStruct+1)->X + (pStruct+1)->Y)/2;
+ 
+    printf("\nX: %d \nY: %d\n", pStruct->X, pStruct->Y);
+    printf("MEDIA: %.2f\n", pStruct->Z);
+
+    return 0;
+}
